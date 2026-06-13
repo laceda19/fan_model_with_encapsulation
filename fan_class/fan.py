71c802fa7365c9cbd58696c08d18c9c6a4a531f9
laceda19 <gelailaceda@gmail.com>
2026-06-13 09:44:32 -0700

    add getter and setters

--- fan_class/fan.py
+++ fan_class/fan.py
@@ -9,3 +9,30 @@ class Fan:
         self.__on = False
         self.__radius = 5
         self.__color = "blue"
+
+    def get_speed(self):
+        return self.__speed
+
+    def get_on(self):
+        return self.__on
+
+    def get_radius(self):
+        return self.__radius
+
+    def get_color(self):
+        return self.__color
+
+        # Setters
+
+    def set_speed(self, speed):
+        self.__speed = speed
+
+    def set_on(self, on):
+        self.__on = on
+
+    def set_radius(self, radius):
+        self.__radius = radius
+
+    def set_color(self, color):
+        self.__color = color
+        
