946c2e210ad4335345fe6a192f3322dad91e9851
laceda19 <gelailaceda@gmail.com>
2026-06-13 11:57:29 -0700

    fix the spacing

--- car_speed_simulator/car.py
+++ car_speed_simulator/car.py
@@ -3,22 +3,16 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
-
     def accelerate(self):
         self.__speed += 5
-
     def brake(self):
         self.__speed -= 5
-
     def get_speed(self):
         return self.__speed
-
     def get_make(self):
         return self.__make
-
     def get_year_model(self):
         return self.__year_model
-
     def display_information(self):
         print("Year Model:", self.__year_model)
         print("Make:", self.__make)
