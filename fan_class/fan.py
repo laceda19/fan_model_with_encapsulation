fd1ae8a9ce3de698b17d8b02b625ed2cb08824d3
laceda19 <gelailaceda@gmail.com>
2026-06-13 09:41:07 -0700

    add private attributes with default values

--- fan_class/fan.py
+++ fan_class/fan.py
@@ -3,3 +3,9 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+
+    def __init__(self):
+        self.__speed = Fan.SLOW
+        self.__on = False
+        self.__radius = 5
+        self.__color = "blue"
